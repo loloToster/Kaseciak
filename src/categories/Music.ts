db29b35483441513cede7615d5b31b4b54bf99b5
loloToster <lolo.toster4@gmail.com>
2022-12-16 13:48:29 +0100

    ✨ added more basic commands

--- src/categories/Music.ts
+++ src/categories/Music.ts
@@ -128,4 +128,151 @@ export class Music {
       await replyHandler.reply(`❌ | Nie znalazłem piosenki **${query}**!`)
     }
   }
+
+  @DualCommand({
+    aliases: ["s"],
+    description: "Skipuje obecną lub kilka piosenek"
+  })
+  async skip(
+    @SimpleCommandOption({
+      name: "ilosc",
+      type: SimpleCommandOptionType.Number
+    })
+    @SlashOption({
+      name: "ilosc",
+      description: "ilość piosenek do skipnięcia",
+      type: ApplicationCommandOptionType.Integer,
+      required: false
+    })
+      amount: number | undefined,
+      interactionOrMsg: CommandInteraction | SimpleCommandMessage
+  ) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    if (!queue || !queue.playing)
+      return replyHandler.reply("Nie ma czego skipnąć")
+
+    if (amount !== undefined && amount > 1) {
+      if (amount > queue.tracks.length) amount = queue.tracks.length
+      queue.skipTo(amount - 1)
+      await replyHandler.reply(`Skipuje **${amount}** piosenek`)
+      return
+    }
+
+    const success = queue.skip()
+
+    await replyHandler.reply(
+      success ? `Skipuje **${queue.current.title}**` : "Coś się pokićkało"
+    )
+  }
+
+  @DualCommand({
+    aliases: ["prev", "previous"],
+    description: "Cofa do poprzedniej piosenki"
+  })
+  async back(interactionOrMsg: CommandInteraction | SimpleCommandMessage) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    await queue?.back()
+
+    await replyHandler.reply("Cofam")
+  }
+
+  @DualCommand({ description: "Pauzuje piosenkę" })
+  async pause(interactionOrMsg: CommandInteraction | SimpleCommandMessage) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    queue?.setPaused(true)
+
+    await replyHandler.reply("Pauzuję")
+  }
+
+  @DualCommand({ description: "Kontunuuje odtwarzanie piosenki" })
+  async resume(interactionOrMsg: CommandInteraction | SimpleCommandMessage) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    queue?.setPaused(false)
+
+    await replyHandler.reply("Wznawiam")
+  }
+
+  @DualCommand({ description: "Przewija piosenkę do konkretnego momentu" })
+  async seek(
+    @SimpleCommandOption({
+      name: "sekundy",
+      type: SimpleCommandOptionType.Number
+    })
+    @SlashOption({
+      name: "sekundy",
+      description: "do której sekundy ma być przewinięta piosenka",
+      type: ApplicationCommandOptionType.Integer,
+      required: true
+    })
+      seconds: number | undefined,
+      interactionOrMsg: CommandInteraction | SimpleCommandMessage
+  ) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    if (typeof seconds !== "number")
+      return await replyHandler.reply("Podaj sekundy")
+
+    const secsInMs = seconds * 1000
+
+    if (queue && (await queue.seek(secsInMs))) {
+      await replyHandler.reply(`Przewijam do ${seconds} sekund`)
+    } else {
+      await replyHandler.reply("Nie zdołałem przewinąć")
+    }
+  }
+
+  @DualCommand({ description: "Czyści kolejkę" })
+  async clear(interactionOrMsg: CommandInteraction | SimpleCommandMessage) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    queue?.clear()
+
+    await replyHandler.reply("Kolejka wyczyszczona 🗑️")
+  }
+
+  @DualCommand({ description: "Tasuje kolejkę" })
+  async shuffle(interactionOrMsg: CommandInteraction | SimpleCommandMessage) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    queue?.shuffle()
+
+    await replyHandler.reply("Zshufflowałem piosenki 🔀")
+  }
+
+  @DualCommand({ description: "Zatrzymuje bota i kasuje kolejke" })
+  async stop(interactionOrMsg: CommandInteraction | SimpleCommandMessage) {
+    const replyHandler = getReplyHandler(interactionOrMsg)
+    if (!replyHandler.guild) return
+
+    const queue = this.player.getQueue(replyHandler.guild)
+
+    queue?.destroy(true)
+
+    await replyHandler.reply("Zatrzymałem i skasowałem kolejke")
+  }
 }
